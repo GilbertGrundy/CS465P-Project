fdb19534b01561a178f8e7b14e19c9ba479adaf1
Gilbert <gilbert.grundy@gmail.com>
2021-03-06 18:54:48 -0800

    Added text for empty table

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function getPerformanceData() {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "http://127.0.0.1:5000/submitPerformances");
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  console.log(xhr.readyState);
+  
   xhr.onreadystatechange = function () {
     // Call a function when the state changes.
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
@@ -26,7 +26,8 @@ function getPerformanceData() {
       ) {
         document.getElementById("womensPerformanceTable").deleteRow(i - 1);
       }
-
+      let mencount = 0;
+      let womencount = 0;
       //insert performance data into the performance.html mens/womens tables
       for (let i = 0; i < data.length; i++) {
         let tempRow;
@@ -37,11 +38,13 @@ function getPerformanceData() {
           mensTableRowCount = document.getElementById("mensPerformanceTable")
             .rows.length;
           tempRow = mensTable.insertRow(mensTableRowCount);
+          mencount++;
         } else {
           womensTableRowCount = document.getElementById(
             "womensPerformanceTable"
           ).rows.length;
           tempRow = womensTable.insertRow(womensTableRowCount);
+          womencount++;
         }
         let tempCell1 = tempRow.insertCell(0);
         let tempCell2 = tempRow.insertCell(1);
@@ -53,12 +56,38 @@ function getPerformanceData() {
         } else {
           tempCell1.innerHTML = womensTableRowCount + 1;
         }
-        if(data[i].relay == 1) tempCell2.innerHTML = data[i].mark + " (r)";
-        else if(data[i].timetrial == 1) tempCell2.innerHTML = data[i].mark + " +";
-        else tempCell2.innerHTML = data[i].mark;
-        tempCell3.innerHTML = data[i].name;
+        tempCell2.innerHTML = data[i].name;
+        if(data[i].relay == 1) tempCell3.innerHTML = data[i].mark + " (r)";
+        else if(data[i].timetrial == 1) tempCell3.innerHTML = data[i].mark + " +";
+        else tempCell3.innerHTML = data[i].mark;
         tempCell4.innerHTML = data[i].day + " " + data[i].month + " " + data[i].year;
         tempCell5.innerHTML = data[i].venue;
+      }
+      if(mencount == 0){
+        let tempRow = document.getElementById("mensPerformanceTable").insertRow(0);
+        let tempCell1 = tempRow.insertCell(0);
+        let tempCell2 = tempRow.insertCell(1);
+        let tempCell3 = tempRow.insertCell(2);
+        let tempCell4 = tempRow.insertCell(3);
+        let tempCell5 = tempRow.insertCell(4);
+        tempCell1.innerHTML = '#';
+        tempCell2.innerHTML = 'Empty Tub!';
+        tempCell3.innerHTML = '69:69:69.69';
+        tempCell4.innerHTML = '69 Banterbruary 2069';
+        tempCell5.innerHTML = 'Tubville';
+      }
+      if(womencount == 0){
+        let tempRow = document.getElementById("womensPerformanceTable").insertRow(0);
+        let tempCell1 = tempRow.insertCell(0);
+        let tempCell2 = tempRow.insertCell(1);
+        let tempCell3 = tempRow.insertCell(2);
+        let tempCell4 = tempRow.insertCell(3);
+        let tempCell5 = tempRow.insertCell(4);
+        tempCell1.innerHTML = '#';
+        tempCell2.innerHTML = 'Empty Tub!';
+        tempCell3.innerHTML = '69:69:69.69';
+        tempCell4.innerHTML = '69 Banterbruary 2069';
+        tempCell5.innerHTML = 'Tubville';
       }
       document.getElementById("tables").style.visibility = "visible";
     }
@@ -125,22 +154,24 @@ function getRecordData(){
       ) {
         document.getElementById("womensRecordTable").deleteRow(i - 1);
       }
-
       //insert performance data into the performance.html mens/womens tables
+      let mencount = 0;
+      let womencount = 0;
       for (let i = 0; i < data.length; i++) {
         let tempRow;
         let mensTableRowCount;
         let womensTableRowCount;
-
         if (data[i].sex === 0) {
           mensTableRowCount = document.getElementById("mensRecordTable")
             .rows.length;
           tempRow = mensTable.insertRow(mensTableRowCount);
+          mencount++;
         } else {
           womensTableRowCount = document.getElementById(
             "womensRecordTable"
           ).rows.length;
           tempRow = womensTable.insertRow(womensTableRowCount);
+          womencount++;
         }
         let tempCell1 = tempRow.insertCell(0);
         let tempCell2 = tempRow.insertCell(1);
@@ -154,6 +185,24 @@ function getRecordData(){
         if(data[i].relay == 1) tempCell3.innerHTML = data[i].mark + " (r)";
         else if(data[i].timetrial == 1) tempCell3.innerHTML = data[i].mark + " +";
         else tempCell3.innerHTML = data[i].mark;
+      }
+      if(mencount == 0){
+        let tempRow = document.getElementById("mensRecordTable").insertRow(0);
+        let tempCell1 = tempRow.insertCell(0);
+        let tempCell2 = tempRow.insertCell(1);
+        let tempCell3 = tempRow.insertCell(2);
+        tempCell1.innerHTML = '#';
+        tempCell2.innerHTML = 'Empty Tub!';
+        tempCell3.innerHTML = '69:69:69.69';
+      }
+      if(womencount == 0){
+        let tempRow = document.getElementById("womensRecordTable").insertRow(0);
+        let tempCell1 = tempRow.insertCell(0);
+        let tempCell2 = tempRow.insertCell(1);
+        let tempCell3 = tempRow.insertCell(2);
+        tempCell1.innerHTML = '#';
+        tempCell2.innerHTML = 'Empty Tub!';
+        tempCell3.innerHTML = '69:69:69.69';
       }
       document.getElementById("tables").style.visibility = "visible";
     }
@@ -172,20 +221,17 @@ function loadEvents(){
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "http://127.0.0.1:5000/events");
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  console.log(xhr.readyState);
   
   let eventList = document.getElementById("eventDDL");
 
   xhr.onreadystatechange = function () {
     // Call a function when the state changes.
-    console.log("In onreadystatechange");
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
       var length = eventList.options.length;
       for (i = length-1; i >= 0; i--) {
         eventList.options[i].remove();
       }
       let data = JSON.parse(xhr.responseText);
-      console.log("Within IF");
       for (let i = 0; i < data.length; i++) {
         let option = document.createElement('option');
         option.value = data[i].event;
